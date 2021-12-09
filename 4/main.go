6a6cb59591d80cac775772338fcf6640444c20ab
Sasha Sokolov <sashasokolov777@gmail.com>
2021-12-09 19:42:26 +0200

    crawler is finished

--- 4/main.go
+++ 4/main.go
@@ -9,7 +9,6 @@ import (
 	"time"
 )
 
-
 const stringToSearch = "concurrency"
 
 var sites = []string{
@@ -24,31 +23,41 @@ var sites = []string{
 	"https://en.wikipedia.org/wiki/Concurrency_(computer_science)#:~:text=In%20computer%20science%2C%20concurrency%20is,without%20affecting%20the%20final%20outcome.",
 }
 
-
 type SiteData struct {
 	data []byte
 	uri  string
 }
 
 func main() {
-	ctx, cancel := context.WithCancel(context.TODO())
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 	resultsCh := make(chan SiteData, len(sites))
 
-	// TODO your code
+	findData(ctx, cancel, resultsCh)
 
-	// give one second to validate if all other goroutines are closed
 	time.Sleep(time.Second)
 }
 
-// TODO implement function that will execute request function, will validate the output and cancel all other requests when needed page is found
-// and will listen to cancellation signal from context and will exit from the func when will receive it
+func findData(ctx context.Context, cancel context.CancelFunc, ch chan SiteData) {
+	for _, uri := range sites {
+		fmt.Println("Sending request to", uri)
+		go performRequest(ctx, uri, ch)
+	}
 
-// TODO implement function that will perfrom request using the example under
+	for {
+		select {
+		case site := <-ch:
+			if ok := strings.Contains(string(site.data), stringToSearch); ok {
+				fmt.Println(stringToSearch+" string is found in", site.uri)
+				cancel()
+				return
+			}
+			fmt.Println("Nothing found in", site.uri)
+		}
+	}
+}
 
-// TODO hint request function code:
-/*
-	Code to make request and read data
-
+func performRequest(ctx context.Context, uri string, ch chan SiteData) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -66,5 +75,5 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-
-/*
+	ch <- SiteData{data: bodyBytes, uri: uri}
+}
